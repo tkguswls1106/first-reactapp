e39d3284ea94e254d499c8d4f174e0f539ac9dca
sahyunjin <tkguswls1106@naver.com>
2022-11-02 22:13:25 +0900

    인프런 '(실습) 댓글 컴포넌트 만들기' 강의에서 기본적인 댓글 컴포넌트 만들고 화면에 출력까지 완료.

--- src/index.js
+++ src/index.js
@@ -6,18 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import CommentList from './\bchapter_05/CommentList';
 
-setInterval(
-  () => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <Clock />
-      </React.StrictMode>,
-      document.getElementById('root')
-    );
-  }, 1000);
+ReactDOM.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

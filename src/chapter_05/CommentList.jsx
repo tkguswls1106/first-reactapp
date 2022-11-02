b25668cbe5e4302658b8b73e49469e4e4811ca7f
sahyunjin <tkguswls1106@naver.com>
2022-11-02 22:36:38 +0900

    그 다음으로, CommentList 컴포넌트에 해당 props 속성값들 하나씩 추가로 더 넣어서 총 사람2명 댓글2개를 화면 출력에 성공.

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -5,6 +5,7 @@ function CommentList(props) {
     return (
         <div>
             <Comment name={"사현진"} comment={"제가 만든 첫 컴포넌트입니다. 참고로 댓글입니다."} />
+            <Comment name={"유재석"} comment={"다른 사람 이름의 다른 댓글 적어보았습니다."} />
         </div>
     );
 }

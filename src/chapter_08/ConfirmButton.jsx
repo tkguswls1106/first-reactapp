d0fedf76e32366d4dec7bb6fe6e4f184c0155b56
sahyunjin <tkguswls1106@naver.com>
2022-11-29 00:13:04 +0900

    그 다음으로, 함수 컴포넌트로 '함수 안에 함수로 정의' 방법을 사용하는 코드로 변경하여 클릭 이벤트 처리하기 코드 작성 후 화면에 출력까지 완료.

--- src/chapter_08/ConfirmButton.jsx
+++ src/chapter_08/ConfirmButton.jsx
@@ -1,30 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-class ConfirmButton extends React.Component {
-    constructor(props) {
-        super(props);
+function ConfirmButton(props) {
+    const [isConfirmed, setIsConfirmed] = useState(false);
 
-        this.state = {
-            isConfirmed: false,
-        };
-    }
+    function handleConfirm() {  // 함수 컴포넌트로 '함수 안에 함수로 정의' 방법
+        setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);  // prevState 같은 용도이지 않을까 싶다
+    };
 
-    handleConfirm = ()  => {
-        this.setState((prevState) => ({  // setState 함수 사용 시 이전 state 값을 사용하고 싶으면 prevState를 이용하면 된다.
-            isConfirmed: !prevState.isConfirmed,  // 이벤트 핸들러로 handleConfirm()가 실행되면, 이전의 state를 불러와 !로 반전시키고 그걸 state로 덮어씌우는것이다.
-        }));
-    }  // prevState는 마치 c언어에서 num = 1 만 반복 선언하다가, num = num + 1 로 바꿔 선언한 느낌이랄까?
-
-    render() {
-        return (
-            <button
-                onClick = {this.handleConfirm}
-                disabled = {this.state.isConfirmed}
-            >
-                {this.state.isConfirmed ? "확인됨" : "확인하기"}
-            </button>
-        );
-    }
+    return (
+        <button onClick={handleConfirm} disabled={isConfirmed}>
+            {isConfirmed ? "확인됨" : "확인하기"}
+        </button>
+    );
 }
 
 export default ConfirmButton;

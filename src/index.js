48f579f3b350d1657c36bc953e7707d0a9edcc35
sahyunjin <tkguswls1106@naver.com>
2023-01-05 00:56:37 +0900

    인프런 React.js 강의 '(실습) 사용자 정보 입력 받기'까지 시청 완료함.

--- src/index.js
+++ src/index.js
@@ -6,16 +6,17 @@ import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
-import CommentList from './\bchapter_05/CommentList';
+import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <SignUp />
   </React.StrictMode>,
   document.getElementById('root')
 );
